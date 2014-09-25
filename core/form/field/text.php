7ba3685b67c998ab99b4e7a9a1d870e74789ae7d
Nimja <nimja.com>
2014-09-25 17:07:39 +0200

    Added rows support to text field.

--- core/form/field/text.php
+++ core/form/field/text.php
@@ -9,8 +9,11 @@ class Text extends \Core\Form\Field
     protected function _getHtml()
     {
         $this->_extra = $this->_addClass($this->_extra, 'form-control');
+        if (!isset($this->_extra['rows'])) {
+            $this->_extra['rows'] = 3;
+        }
         return sprintf(
-            '<textarea name="%s" %s rows="3">%s</textarea>',
+            '<textarea name="%s" %s>%s</textarea>',
             $this->name,
             $this->_extra($this->_extra),
             $this->value
