6ba16e7c4c2268bc442b531a122e9ac6d4509b32
Nimja <nimja.com>
2015-08-04 13:07:59 +0200

    Rewrote image class and moved resize functionality.

--- core/file/image.php
+++ core/file/image.php
@@ -1,186 +1,129 @@
-<?php
-namespace Core\File;
-//Loading an image file.
-class Image {
-	const MODE_RATIO_CROPPING = 0;
-	const MODE_RATIO_FITTING = 1;
-	const MODE_FORCE_WIDTH = 2;
-	const MODE_FORCE_HEIGHT = 3;
-	const MODE_RATIO_FITTING_NO_BORDERS = 4;
+<?php namespace Core\File;
+/**
+ * Image class, that wraps around loading an image and makes it easier.
+ */
+class Image
+{
+    const TYPE_PNG = 'png';
+    const TYPE_JPEG = 'jpg';
+    /**
+     * Current type, for output.
+     * @var string
+     */
+    public $type;
+    /**
+     * Current image object.
+     * @var resource an image resource identifier on success
+     */
+    public $img;
+    /**
+     * Width of the image, in pixels.
+     * @var int
+     */
+    public $width = 0;
+    /**
+     * Height of the image, in pixels.
+     * @var int
+     */
+    public $height = 0;
+    /**
+     * Megapixels of this image.
+     * @var float
+     */
+    public $mp = 0;
 
+    /**
+     * Create image, always need a filename.
+     * @param string|int $fileName Or width if creating a blank image.
+     * @param int
+     * @param string $type
+     * @throws Exception
+     */
+    public function __construct($fileName, $height = 0, $type = self::TYPE_PNG)
+    {
+        if (empty($height)) {
+            $this->_load($fileName);
+        } else if (is_numeric($fileName) && $height > 0) {
+            $this->_create($fileName, $height);
+            $this->setType($type);
+        } else {
+            throw new \Exception("Wrong parameters?");
+        }
+    }
 
-	var $src = NULL;
-	var $img = NULL;
-	var $size = NULL;
-	var $mp = NULL;
-	var $error = FALSE;
+    /**
+     * Create new image.
+     * @param mixed $width
+     * @param mixed $height
+     */
+    private function _create($width, $height)
+    {
+        $this->width = intval($width);
+        $this->height = intval($height);
+        $this->img = imagecreatetruecolor($this->width, $this->height);
+        $this->_calculateMp();
+    }
 
-	function __construct()
-	{
-		$this->error = FALSE;
-	}
+    /**
+     * Load image from file.
+     * @return boolean
+     */
+    private function _load($fileName)
+    {
+        if (!file_exists($fileName)) {
+            throw new \Exception("Image not found; $fileName");
+        }
+        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+        #Load image
+        switch ($extension) {
+            case 'png': $this->img = imagecreatefrompng($fileName);
+                break;
+            case 'jpg':
+            case 'jpeg': $this->img = imagecreatefromjpeg($fileName);
+                break;
+            case 'gif': $this->img = imagecreatefromgif($fileName);
+                break;
+        }
+        if ($this->img === false) {
+            throw new \Exception("Unable to load image.");
+        }
+        $size = getimagesize($fileName);
+        $this->width = $size[0];
+        $this->height = $size[1];
+        $this->_calculateMp();
+    }
 
-	public function load($src)
-	{
-		$img = null;
+    /**
+     * Calculate megapixels in the image, mostly for fun.
+     * @return void
+     */
+    private function _calculateMp()
+    {
+        $this->mp = round($this->width * $this->height / 1000000, 1);
+    }
 
-		$parts = pathinfo($src);
-		$ext = $parts['extension'];
-		#Load image
-		switch ($ext) {
-			case 'png': $img = imagecreatefrompng($src) or \Show::error($src, 'PNG could not be loaded, corrupt?');
-				break;
-			case 'jpg':
-			case 'jpeg': $img = imagecreatefromjpeg($src) or \Show::error($src, 'JPG could not be loaded, corrupt?');
-				break;
-			case 'gif': $img = imagecreatefromgif($src) or \Show::error($src, 'GIF could not be loaded, corrupt?');
-				break;
-		}
-		$this->src = $src;
-		$this->img = $img;
-		if (empty($img)) {
-			\Show::error($src, 'File is not an image.');
-			return;
-		}
-		$size = getimagesize($src);
-		$this->size = $size;
-		$this->mp = floor($size[0] * $size[1] / 100000) / 10;
-	}
+    public function setType($type)
+    {
+        $types = array(self::TYPE_PNG, self::TYPE_JPEG);
+        if (!in_array($type, $types)) {
+            throw new \Exception("Wrong type given: $type");
+        }
+        $this->type = $type;
+    }
 
-	private function fastimagecopyresampled(&$dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h, $quality = 3)
-	{
-		# Plug-and-Play fastimagecopyresampled function replaces much slower imagecopyresampled.
-		# Just include this function and change all "imagecopyresampled" references to "fastimagecopyresampled".
-		# Typically from 30 to 60 times faster when reducing high resolution images down to thumbnail size using the default quality setting.
-		# Author: Tim Eckel - Date: 09/07/07 - Version: 1.1 - Project: FreeRingers.net - Freely distributable - These comments must remain.
-		#
-		# Optional "quality" parameter (defaults is 3). Fractional values are allowed, for example 1.5. Must be greater than zero.
-		# Between 0 and 1 = Fast, but mosaic results, closer to 0 increases the mosaic effect.
-		# 1 = Up to 350 times faster. Poor results, looks very similar to imagecopyresized.
-		# 2 = Up to 95 times faster.	Images appear a little sharp, some prefer this over a quality of 3.
-		# 3 = Up to 60 times faster.	Will give high quality smooth results very close to imagecopyresampled, just faster.
-		# 4 = Up to 25 times faster.	Almost identical to imagecopyresampled for most images.
-		# 5 = No speedup. Just uses imagecopyresampled, no advantage over imagecopyresampled.
-
-		if (empty($src_image) || empty($dst_image) || $quality <= 0) {
-			return false;
-		}
-		if ($quality < 5 && (($dst_w * $quality) < $src_w || ($dst_h * $quality) < $src_h)) {
-			$temp = imagecreatetruecolor($dst_w * $quality + 1, $dst_h * $quality + 1);
-			imagecopyresized($temp, $src_image, 0, 0, $src_x, $src_y, $dst_w * $quality + 1, $dst_h * $quality + 1, $src_w, $src_h);
-			imagecopyresampled($dst_image, $temp, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $dst_w * $quality, $dst_h * $quality);
-			imagedestroy($temp);
-		} else
-			imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
-		return true;
-	}
-
-	#Resize the loaded image into a different size.
-	#Modes:
-	# 0 = crop (cut edges)
-	# 1 = fit inside the border (fill edges);
-	# 2 = fit horizontally (fill edges);
-	# 3 = fit vertically (fill edges);
-	# 4 = fit (result might be smaller in w/h
-
-	public function resize($src, $dst, $width, $height, $mode = 0, $quality = 75)
-	{
-		if ($src != $this->src) {
-			$this->load($src);
-		}
-		#Sanity check
-		if ($this->error || empty($this->img) || empty($this->size)) {
-			\Show::error('Image not loaded');
-			return;
-		}
-
-		#Desired size
-		$desired = array('w' => $width, 'h' => $height);
-		#original size
-		$source = array('x' => 0, 'y' => 0, 'w' => $this->size[0], 'h' => $this->size[1]);
-
-		#Sanity check.
-		$fileTo = $dst;
-		#Delete if it existed before.
-		if (file_exists($fileTo)) {
-			return;
-		}
-
-		$dest = $source;
-		#Cropping or scaling.
-		$scaleX = $desired['w'] / $source['w'];
-		$scaleY = $desired['h'] / $source['h'];
-		$scale = 1;
-		switch ($mode) {
-			case self::MODE_FORCE_HEIGHT: //Force height
-				$scale = $scaleY;
-				break;
-			case self::MODE_FORCE_WIDTH: //Force width
-				$scale = $scaleX;
-				break;
-			case self::MODE_RATIO_FITTING_NO_BORDERS: //Fit without additional borders.
-			case self::MODE_RATIO_FITTING: //Fit
-				$scale = ($scaleX < $scaleY) ? $scaleX : $scaleY;
-				break;
-			default: //Ratio cropping
-				$scale = ($scaleX > $scaleY) ? $scaleX : $scaleY;
-				break;
-		}
-		#If the destination is larger than the source.
-		if ($scale > 1) {
-			imagejpeg($this->img, $fileTo, $quality);
-			return;
-		}
-		$dest['w'] = floor($source['w'] * $scale);
-		$dest['h'] = floor($source['h'] * $scale);
-
-		$fill = FALSE;
-		if ($mode == 4) {
-			$desired['w'] = $dest['w'];
-			$desired['h'] = $dest['h'];
-		} else {
-			//Horizontal cropping
-			if ($dest['w'] > $desired['w']) {
-				$newW = $desired['w'] / $scale;
-				$source['x'] = ($source['w'] - $newW) / 2;
-				$source['w'] = $newW;
-				$dest['w'] = $desired['w'];
-				//Horizontal fitting
-			} elseif ($dest['w'] < $desired['w']) {
-				$fill = TRUE;
-				$dest['x'] = ($desired['w'] - $dest['w']) / 2;
-			}
-
-			//Vertical cropping
-			if ($dest['h'] > $desired['h']) {
-				$newH = $desired['h'] / $scale;
-				$source['y'] = ($source['h'] - $newH) / 2;
-				$source['h'] = $newH;
-				$dest['h'] = $desired['h'];
-				//Vertical fitting
-			} elseif ($dest['h'] < $desired['h']) {
-				$fill = TRUE;
-				$dest['y'] = ($desired['h'] - $dest['h']) / 2;
-			}
-		}
-
-
-		$dst = imagecreatetruecolor($desired['w'], $desired['h']);
-		// Resample the original image into the resized canvas we set up earlier
-		$speed = '2';
-		$this->fastimagecopyresampled($dst, $this->img, $dest['x'], $dest['y'], $source['x'], $source['y'], $dest['w'], $dest['h'], $source['w'], $source['h'], 2);
-
-		imagejpeg($dst, $fileTo, $quality);
-		chmod($fileTo, 0666);
-		imagedestroy($dst);
-	}
-
-	function clean()
-	{
-		if (!empty($this->img)) {
-			imagedestroy($this->img);
-		}
-		$this->error = FALSE;
-	}
-
+    /**
+     * Return image string, for output to browser.
+     */
+    public function render()
+    {
+        ob_start();
+        if ($this->type == self::TYPE_JPEG) {
+            imagejpeg($this->img);
+        } else {
+            imagepng($this->img);
+        }
+        $result = ob_get_contents();
+        ob_end_clean();
+        return $result;
+    }
 }
---
+++ core/file/image/resize.php
@@ -0,0 +1,244 @@
+<?php namespace Core\File\Image;
+//Loading an image file.
+class Resize
+{
+    const MODE_RATIO_CROPPING = 0;
+    const MODE_RATIO_FITTING = 1;
+    const MODE_FORCE_WIDTH = 2;
+    const MODE_FORCE_HEIGHT = 3;
+    const MODE_RATIO_FITTING_NO_BORDERS = 4;
+    /**
+     * The current mode for resizing.
+     * @var int
+     */
+    private $_mode = self::MODE_RATIO_CROPPING;
+
+    /**
+     * Create the resizer with a mode.
+     *
+     * * MODE_RATIO_CROPPING = crop (cut edges)
+     * * MODE_RATIO_FITTING = fit inside the border (fill edges);
+     * * MODE_FORCE_WIDTH = fit horizontally (fill edges);
+     * * MODE_FORCE_HEIGHT = fit vertically (fill edges);
+     * * MODE_RATIO_FITTING_NO_BORDERS = fit (result might be smaller in w/h
+     * @param int $mode
+     */
+    public function __construct($mode = self::MODE_RATIO_CROPPING)
+    {
+        $this->_mode = $mode;
+    }
+
+    /**
+     * Make sure source is always an image, but allow filenames.
+     * @param mixed $source
+     * @return \Core\File\Image
+     */
+    private function _getImage($source)
+    {
+        return (!$source instanceof \Core\File\Image) ? new \Core\File\Image($source) : $source;
+    }
+
+    /**
+     * Creates a new image as a thumbnail for the other.
+     * @param \Core\File\Image|string $source
+     * @param int $width
+     * @param int $height
+     * @return \Core\File\Image
+     */
+    public function thumbnail($source, $width, $height)
+    {
+        $sourceImage = $this->_getImage($source);
+        $destImage = new \Core\File\Image($width, $height);
+        return $this->resize($sourceImage, $destImage, $width, $height);
+    }
+
+    /**
+     * Plug-and-Play fastimagecopyresampled function replaces much slower imagecopyresampled.
+     * Just include this function and change all "imagecopyresampled" references to "fastimagecopyresampled".
+     * Typically from 30 to 60 times faster when reducing high resolution images down to thumbnail size using the default quality*setting.
+     * Author: Tim Eckel - Date: 09/07/07 - Version: 1.1 - Project: FreeRingers.net - Freely distributable - These comments*must remain.
+     *
+     * Optional "quality" parameter (defaults is 3). Fractional values are allowed, for example 1.5. Must be greater than zero.*
+     * Between 0 and 1 = Fast, but mosaic results, closer to 0 increases the mosaic effect.
+     * 1 = Up to 350 times faster. Poor results, looks very similar to imagecopyresized.
+     * 2 = Up to 95 times faster.	Images appear a little sharp, some prefer this over a quality of 3.
+     * 3 = Up to 60 times faster.	Will give high quality smooth results very close to imagecopyresampled, just faster.
+     * 4 = Up to 25 times faster.	Almost identical to imagecopyresampled for most images.
+     * 5 = No speedup. Just uses imagecopyresampled, no advantage over imagecopyresampled.
+     *
+     * @param resource $dst_image
+     * @param resource $src_image
+     * @param int $dst_x x-coordinate of destination point.
+     * @param int $dst_y y-coordinate of destination point.
+     * @param int $src_x x-coordinate of source point.
+     * @param int $src_y y-coordinate of source point.
+     * @param int $dst_w Destination width.
+     * @param int $dst_h Destination height.
+     * @param int $src_w Source width.
+     * @param int $src_h Source height.
+     * @param type $quality
+     * @return boolean true on success, false on failure
+     */
+    public static function imageCopyResampled(&$dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h,
+        $src_w, $src_h, $quality = 3)
+    {
+        if (empty($src_image) || empty($dst_image) || $quality <= 0) {
+            return false;
+        }
+        if ($quality < 5 && (($dst_w * $quality) < $src_w || ($dst_h * $quality) < $src_h)) {
+            $temp = imagecreatetruecolor($dst_w * $quality + 1, $dst_h * $quality + 1);
+            imagecopyresized($temp, $src_image, 0, 0, $src_x, $src_y, $dst_w * $quality + 1, $dst_h * $quality + 1,
+                $src_w, $src_h);
+            imagecopyresampled($dst_image, $temp, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $dst_w * $quality,
+                $dst_h * $quality);
+            imagedestroy($temp);
+        } else {
+            imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+        }
+        return true;
+    }
+
+    /**
+     * Copy/resize an image into another.
+     * @param \Core\File\Image|string $source
+     * @param \Core\File\Image|string $destination
+     * @param int $width
+     * @param int $height
+     * @param int $x Optional x position of the destination image.
+     * @param int $y Optional y position of the destination image.
+     * @return type
+     */
+    public function resize($source, $destination, $width, $height, $x = 0, $y = 0)
+    {
+        $sourceImage = $this->_getImage($source);
+        $destImage = $this->_getImage($destination);
+
+        $sourceSize = new ResizeData(0, 0, $sourceImage->width, $sourceImage->height);
+        $desiredSize = new ResizeData($x, $y, $width, $height);
+
+        $scale = $this->_calculateScale($sourceSize, $desiredSize);
+        $destSize = $this->_calculateSizes($sourceSize, $desiredSize, $scale);
+
+        self::imageCopyResampled(
+            $destImage->img, $sourceImage->img, $destSize->x + $desiredSize->x, $destSize->y + $desiredSize->y,
+            $sourceSize->x, $sourceSize->y, $destSize->w, $destSize->h, $sourceSize->w, $sourceSize->h, 2
+        );
+        return $destImage;
+    }
+
+    /**
+     * Calculate scale of the image we're resizing.
+     * @param ResizeData $sourceSize
+     * @param ResizeData $desiredSize
+     * @return float
+     */
+    private function _calculateScale($sourceSize, $desiredSize)
+    {
+        #Cropping or scaling.
+        $scaleX = $desiredSize->w / $sourceSize->w;
+        $scaleY = $desiredSize->h / $sourceSize->h;
+        $scale = 1;
+        switch ($this->_mode) {
+            case self::MODE_FORCE_HEIGHT: //Force height
+                $scale = $scaleY;
+                break;
+            case self::MODE_FORCE_WIDTH: //Force width
+                $scale = $scaleX;
+                break;
+            case self::MODE_RATIO_FITTING_NO_BORDERS: //Fit without additional borders.
+            case self::MODE_RATIO_FITTING: //Fit
+                $scale = ($scaleX < $scaleY) ? $scaleX : $scaleY;
+                break;
+            default: //Ratio cropping
+                $scale = ($scaleX > $scaleY) ? $scaleX : $scaleY;
+                break;
+        }
+        return $scale;
+    }
+
+    /**
+     * This function modifies the sizes to the correct result.
+     * @param ResizeData $sourceSize
+     * @param ResizeData $desiredSize
+     * @param float $scale
+     * @return ResizeData
+     */
+    private function _calculateSizes($sourceSize, $desiredSize, $scale)
+    {
+        // Destination size.
+        $destSize = new ResizeData($sourceSize->x, $sourceSize->y, $sourceSize->w, $sourceSize->h);
+
+        $destSize->w = floor($sourceSize->w * $scale);
+        $destSize->h = floor($sourceSize->h * $scale);
+
+        if ($this->_mode == self::MODE_RATIO_FITTING_NO_BORDERS) {
+            $desiredSize->w = $destSize->w;
+            $desiredSize->h = $destSize->h;
+        } else {
+            //Horizontal cropping
+            if ($destSize->w > $desiredSize->w) {
+                $newW = $desiredSize->w / $scale;
+                $sourceSize->x = ($sourceSize->w - $newW) / 2;
+                $sourceSize->w = $newW;
+                $destSize->w = $desiredSize->w;
+                //Horizontal fitting
+            } elseif ($destSize->w < $desiredSize->w) {
+                $destSize->x = ($desiredSize->w - $destSize->w) / 2;
+            }
+
+            //Vertical cropping
+            if ($destSize->h > $desiredSize->h) {
+                $newH = $desiredSize->h / $scale;
+                $sourceSize->y = ($sourceSize->h - $newH) / 2;
+                $sourceSize->h = $newH;
+                $destSize->h = $desiredSize->h;
+                //Vertical fitting
+            } elseif ($destSize->h < $desiredSize->h) {
+                $destSize->y = ($desiredSize->h - $destSize->h) / 2;
+            }
+        }
+        return $destSize;
+    }
+}
+
+/**
+ * Very small class just for holding data.
+ */
+class ResizeData
+{
+    /**
+     * X coordinate.
+     * @var int
+     */
+    public $x;
+    /**
+     * Y coordinate.
+     * @var int
+     */
+    public $y;
+    /**
+     * Width.
+     * @var int
+     */
+    public $w;
+    /**
+     * Height.
+     * @var int
+     */
+    public $h;
+
+    /**
+     * Basic constructor.
+     * @param int $x
+     * @param int $y
+     * @param int $w
+     * @param int $h
+     */
+    public function __construct($x, $y, $w, $h)
+    {
+        $this->x = intval($x);
+        $this->y = intval($y);
+        $this->w = intval($w);
+        $this->h = intval($h);
+    }
+}
