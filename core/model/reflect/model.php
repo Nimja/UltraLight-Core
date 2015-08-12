418b88fabc605beef3f527916df82ff37d222921
Nimja <nimja.com>
2015-08-12 09:49:49 +0200

    Better support for multiple DB usage.

--- core/model/reflect/model.php
+++ core/model/reflect/model.php
@@ -32,11 +32,10 @@ class Model {
             throw new \Exception("Reflecting non model class: $class");
         }
         $this->_reflect = new \Core\Model\Reflect();
-        $short = str_replace('\\', '_', $class);
-        $this->_setTableName($short);
-        $this->_reflect->type = strtolower($short);
         $this->_class = new \ReflectionClass($class);
-        $this->_setDbName();
+        $short = str_replace('\\', '_', $class);
+        $this->_setTableName($short, $this->_setDbName());
+        $this->_reflect->type = strtolower($short);
         $this->_getSettings();
         unset($this->_class);
     }
@@ -52,14 +51,21 @@ class Model {
         return $this->_reflect;
     }
 
+    /**
+     * Set DB name and return prefix, if any.
+     * @return string
+     */
     private function _setDbName()
     {
         $doc = self::parseDocComment($this->_class->getDocComment());
         $dbName = null;
+        $prefix = '';
         if (!empty($doc)) {
             $dbName = getKey($doc, 'db-database');
+            $prefix = getKey($doc, 'db-prefix', '');
         }
         $this->_reflect->dbName = $dbName;
+        return $prefix;
     }
 
     /**
@@ -80,14 +86,25 @@ class Model {
     }
 
     /**
-     * Get the table name.
+     * Set the table name in the reflect object.
+     *
+     * If possible, this will also add the database name, for multiple connection situations.
      * @param string $shortName
+     * @param string $reflectPrefix
      * @return string
      */
-    private function _setTableName($shortName)
+    private function _setTableName($shortName, $reflectPrefix)
     {
-        $prefix = \Config::system()->get('database', 'table_prefix', '');
-        $this->_reflect->table = $prefix . $shortName;
+        $prefix = $reflectPrefix ?: \Config::system()->get('database', 'table_prefix', '');
+        $tableName = $prefix . $shortName;
+        $connection = \Config::system()->get('database', 'connection');
+        if ($connection && isset($connection[$this->_reflect->dbName])) {
+            $dbName = getKey($connection[$this->_reflect->dbName], 'database');
+            if ($dbName) {
+                $tableName = "{$dbName}.{$tableName}";
+            }
+        }
+        $this->_reflect->table = $tableName;
     }
 
     /**
