c6994b64d92295d3396d70227790d19a900f1cd0
Nimja <nimja.com>
2016-03-11 16:09:40 +0100

    Added date formatting to value field.

--- core/form/field/value.php
+++ core/form/field/value.php
@@ -5,13 +5,20 @@ namespace Core\Form\Field;
  */
 class Value extends \Core\Form\Field
 {
-
+    const TYPE_DATE = 'date';
+    
     protected function _getHtml()
     {
         $values = $this->_getValues();
         $value = $this->value;
         if ($values) {
             $value = isset($values[$value]) ? $values[$value] : $value;
+        }
+        if (!empty($this->_extra['type'])) {
+            if ($this->_extra['type'] == self::TYPE_DATE) {
+                $value = date('Y-m-d', $value);
+            }
+            unset($this->_extra['type']);
         }
         return sprintf(
             '<p class="form-control-static" data-name="%s" %s >%s</p>',
