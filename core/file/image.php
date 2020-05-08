f91d9f4f51791b76eabdd3d31fdb73e49c099197
Remy <remy@emesa.nl>
2020-05-08 08:04:09 +0200

    Added quality support to image.

--- core/file/image.php
+++ core/file/image.php
@@ -27,6 +27,11 @@ class Image
      */
     public $height = 0;
     /**
+     * JPeg quality of the output, default 75.
+     * @var int
+     */
+    public $quality = 75;
+    /**
      * Megapixels of this image.
      * @var float
      */
@@ -118,7 +123,7 @@ class Image
     {
         ob_start();
         if ($this->type == self::TYPE_JPEG) {
-            imagejpeg($this->img);
+            imagejpeg($this->img, null, $this->quality);
         } else {
             imagepng($this->img);
         }
